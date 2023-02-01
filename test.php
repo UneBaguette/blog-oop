18afc9769cb63e8d0579f3145534015c2329b22d
UneBaguette <28904802+UneBaguette@users.noreply.github.com>
2023-02-01 11:01:07 +0100

    changes

--- test.php
+++
@@ -1,8 +0,0 @@
-<?php
-require_once __DIR__ . "/vendor/autoload.php";
-require __DIR__ . '/app/Models/User.php';
-
-$db = new Database\DBConnection("blog_oop", "127.0.0.1", "root", "");
-
-$cu = new App\models\User($db);
-$cu::test();
